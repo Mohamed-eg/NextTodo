d6f4ec216606f091a827b7c75aea99a171e25416
Mohamed-eg <83299842+Mohamed-eg@users.noreply.github.com>
2023-11-03 15:46:13 +0200

    up.env

--- src/app/componants/TodoItem.tsx
+++ src/app/componants/TodoItem.tsx
@@ -13,6 +13,7 @@ export function TodoItems ({ id, title, complete, toggleTodo }: TodoItemProos){
             onChange={e=> toggleTodo(id , e.target.checked)} className="cursor-pointer peer "/>
             <label htmlFor={id} 
             className=" cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+            <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Badge</span>
         </li>
     
 }
